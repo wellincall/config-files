b6fa659ab0a6722c4ead6dc0ddf62605a2ab93aa
Wellington Calligaris <wcalligaris@gmail.com>
2024-07-04 11:52:38 +0200

    Updating init file in neovim inspired by Primeagen

--- init.lua
+++ init.lua
@@ -48,8 +48,25 @@ require("neo-tree").setup({
 
 require("telescope")
 
+-- updating leader key
+vim.g.mapleader = " "
+
 -- cursor line
 vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
+-- copying content internally/externally
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- scrolling
+vim.opt.scrolloff = 8
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- moving selection around file
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- numbers
 vim.opt.number = true
@@ -69,6 +86,9 @@ vim.opt.splitright = true
 -- "smart search"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- reference for line length
 vim.opt.colorcolumn = "80"
@@ -84,3 +104,6 @@ vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
 
 -- using compact mode for CtrlSF
 vim.g.ctrlsf_default_view_mode = "compact"
+
+-- substitute current selection
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
